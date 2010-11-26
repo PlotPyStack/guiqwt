e66ebb7b8676b469bb59438a0db3eea6c9663a40
Pierre Raybaut <pierre.raybaut@cea.fr>
2010-11-26 19:11:54 +0100

    version

--- guiqwt/__init__.py
+++ guiqwt/__init__.py
@@ -536,7 +536,7 @@ External resources:
 #    Version 2.0 dated 2006-09-05.
 
 
-__version__ = '2.0.6'
+__version__ = '2.0.7'
 
 # Dear (Debian, RPM, ...) package makers, please feel free to customize the
 # following path to module's data (images) and translations:
