4c2c69582af1ce1b6e3b047327a2f67cbab99e93
Pierre Raybaut <pierre.raybaut@gmail.com>
2015-12-03 15:51:46 +0100

    build_dist.bat: do not remove build and dist folders

--- build_dist.bat
+++ build_dist.bat
@@ -1,6 +1,4 @@
 del MANIFEST
-rmdir /S /Q build
-rmdir /S /Q dist
 python setup.py build sdist
 python setup.py --no-user-cfg build -c msvc bdist_wininst --sse2
 python setup.py --no-user-cfg build -c msvc bdist_wheel --sse2
