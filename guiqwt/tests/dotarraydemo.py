e73780e4eeb70c510804b7b00bd14d37f86848ed
Pierre Raybaut <pierre.raybaut@gmail.com>
2016-01-05 15:46:34 +0100

    Update dotarraydemo.py
    Added a logo in the bottom-right corner of the main window.

--- guiqwt/tests/dotarraydemo.py
+++ guiqwt/tests/dotarraydemo.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2009-2010 CEA
+# Copyright © 2016 CEA
 # Pierre Raybaut
 # Licensed under the terms of the CECILL License
 # (see guiqwt/__init__.py for details)
+
 """
 Dot array example
 =================
@@ -17,32 +18,35 @@ associated dialog box).
 
 SHOW = True # Show test in GUI-based test launcher
 
-from guidata.qt.QtCore import QPointF, QRectF
-from guidata.qt.QtGui import QPen, QBrush, QColor, QMessageBox, QPainter
+import numpy as np
 
-from guidata.dataset.datatypes import DataSet, BeginGroup, EndGroup
-from guidata.dataset.dataitems import FloatItem, ColorItem
-from guidata.dataset.qtwidgets import DataSetEditGroupBox
+import guidata.qt.QtCore as QtCore
+import guidata.qt.QtGui as QtGui
 
-from guiqwt.plot import ImageDialog
-from guiqwt.curve import vmap
-from guiqwt.image import RawImageItem, IImageItemType
-from guiqwt.tools import SaveAsTool, CopyToClipboardTool, PrintTool, HelpTool
+import guidata.dataset.datatypes as gdt
+import guidata.dataset.dataitems as gdi
+import guidata.dataset.qtwidgets as gdq
+import guidata.configtools as configtools
+
+import guiqwt.plot as gqp
+import guiqwt.curve as gqc
+import guiqwt.image as gqi
+import guiqwt.tools as gqt
 
 
-class DotArrayParam(DataSet):
+class DotArrayParam(gdt.DataSet):
     """Dot array"""
-    g1 = BeginGroup("Size of the area")
-    dim_h = FloatItem("Width", default=20, min=0, unit="mm")
-    dim_v = FloatItem("Height", default=20, min=0, unit="mm")
-    _g1 = EndGroup("Size of the area")
+    g1 = gdt.BeginGroup("Size of the area")
+    dim_h = gdi.FloatItem("Width", default=20, min=0, unit="mm")
+    dim_v = gdi.FloatItem("Height", default=20, min=0, unit="mm")
+    _g1 = gdt.EndGroup("Size of the area")
 
-    g2 = BeginGroup("Grid pattern properties")
-    step_x = FloatItem("Step in X-axis", default=1, min=1, unit="mm")
-    step_y = FloatItem("Step in Y-axis", default=1, min=1, unit="mm")
-    size = FloatItem("Dot size", default=.2, min=0, max=2, slider=True, unit="mm")
-    color = ColorItem("Dot color", default="red")
-    _g2 = EndGroup("Grid pattern properties")
+    g2 = gdt.BeginGroup("Grid pattern properties")
+    step_x = gdi.FloatItem("Step in X-axis", default=1, min=1, unit="mm")
+    step_y = gdi.FloatItem("Step in Y-axis", default=1, min=1, unit="mm")
+    size = gdi.FloatItem("Dot size", default=.2, min=0, max=2, slider=True, unit="mm")
+    color = gdi.ColorItem("Dot color", default="red")
+    _g2 = gdt.EndGroup("Grid pattern properties")
     
     def update_image(self, obj):
         self._update_cb()
@@ -51,7 +55,7 @@ class DotArrayParam(DataSet):
         pass
 
 
-class DotArrayItem(RawImageItem):
+class DotArrayItem(gqi.RawImageItem):
     def __init__(self, imageparam=None):
         super(DotArrayItem, self).__init__(np.zeros((1, 1)), imageparam)
         self.update_border()
@@ -59,31 +63,31 @@ class DotArrayItem(RawImageItem):
     def boundingRect(self):
         param = self.imageparam
         if param is not None:
-            return QRectF(QPointF(0, 0),
-                          QPointF(param.dim_h+param.size,
-                                  param.dim_v+param.size))
+            return QtCore.QRectF(QtCore.QPointF(0, 0),
+                                 QtCore.QPointF(param.dim_h+param.size,
+                                                param.dim_v+param.size))
 
     def types(self):
-        return (IImageItemType,)
+        return (gqi.IImageItemType,)
 
     def draw_image(self, painter, canvasRect, srcRect, dstRect, xMap, yMap): 
-        painter.setRenderHint(QPainter.Antialiasing, True)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing, True)
         param = self.imageparam
-        xcoords = vmap(xMap, np.arange(0, param.dim_h+1, param.step_x))
-        ycoords = vmap(yMap, np.arange(0, param.dim_v+1, param.step_y))
+        xcoords = gqc.vmap(xMap, np.arange(0, param.dim_h+1, param.step_x))
+        ycoords = gqc.vmap(yMap, np.arange(0, param.dim_v+1, param.step_y))
         rx = .5*param.size*xMap.pDist()/xMap.sDist()
         ry = .5*param.size*yMap.pDist()/yMap.sDist()
-        color = QColor(param.color)
-        painter.setPen(QPen(color))
-        painter.setBrush(QBrush(color))
+        color = QtGui.QColor(param.color)
+        painter.setPen(QtGui.QPen(color))
+        painter.setBrush(QtGui.QBrush(color))
         for xc in xcoords:
             for yc in ycoords:
-                painter.drawEllipse(QPointF(xc, yc), rx, ry)
+                painter.drawEllipse(QtCore.QPointF(xc, yc), rx, ry)
 
 
-class CustomHelpTool(HelpTool):
+class CustomHelpTool(gqt.HelpTool):
     def activate_command(self, plot, checked):
-        QMessageBox.information(plot, "Help",
+        QtGui.QMessageBox.information(plot, "Help",
                                 """**to be customized**
 Keyboard/mouse shortcuts:
   - single left-click: item (curve, image, ...) selection
@@ -95,7 +99,7 @@ Keyboard/mouse shortcuts:
   - right-click + mouse move: zoom""")
 
 
-class DotArrayDialog(ImageDialog):
+class DotArrayDialog(gqp.ImageDialog):
     def __init__(self):
         self.item = None
         self.stamp_gbox = None
@@ -107,9 +111,9 @@ class DotArrayDialog(ImageDialog):
     def register_tools(self):
         self.register_standard_tools()
         self.add_separator_tool()
-        self.add_tool(SaveAsTool)
-        self.add_tool(CopyToClipboardTool)
-        self.add_tool(PrintTool)
+        self.add_tool(gqt.SaveAsTool)
+        self.add_tool(gqt.CopyToClipboardTool)
+        self.add_tool(gqt.PrintTool)
         self.add_tool(CustomHelpTool)
         self.activate_default_tool()
         plot = self.get_plot()
@@ -117,7 +121,15 @@ class DotArrayDialog(ImageDialog):
         plot.set_aspect_ratio(lock=True)
 
     def create_plot(self, options):
-        self.stamp_gbox = DataSetEditGroupBox("Dots", DotArrayParam)
+        logo_path = configtools.get_image_file_path("guiqwt.svg")
+        logo = QtGui.QLabel()
+        logo.setPixmap(QtGui.QPixmap(logo_path))
+        logo.setAlignment(QtCore.Qt.AlignCenter)
+        self.plot_layout.addWidget(logo, 1, 1)
+        logo_txt = QtGui.QLabel("Powered by <b>guiqwt</b>")
+        logo_txt.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
+        self.plot_layout.addWidget(logo_txt, 2, 1)
+        self.stamp_gbox = gdq.DataSetEditGroupBox("Dots", DotArrayParam)
         try:
             # guiqwt v3:
             self.stamp_gbox.SIG_APPLY_BUTTON_CLICKED.connect(self.apply_params)
@@ -128,7 +140,7 @@ class DotArrayDialog(ImageDialog):
                          self.apply_params)
         self.plot_layout.addWidget(self.stamp_gbox, 0, 1)
         options = dict(title="Main plot")
-        ImageDialog.create_plot(self, options, 0, 0, 2, 1)
+        gqp.ImageDialog.create_plot(self, options, 0, 0, 3, 1)
         
     def show_data(self, param):
         plot = self.get_plot()
@@ -136,6 +148,8 @@ class DotArrayDialog(ImageDialog):
             param._update_cb = lambda: self.stamp_gbox.get()
             self.item = DotArrayItem(param)
             plot.add_item(self.item)
+        else:
+            self.item.update_border()
         plot.do_autoscale()
         
     def apply_params(self):
@@ -146,7 +160,6 @@ class DotArrayDialog(ImageDialog):
 if __name__ == "__main__":
     # -- Create QApplication
     import guidata
-    import numpy as np
     _app = guidata.qapplication()
     
     dlg = DotArrayDialog()
