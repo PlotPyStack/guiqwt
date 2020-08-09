5d809c1cc181fa9622b678542f08d720b356e52c
Pierre Raybaut <pierre.raybaut@gmail.com>
2020-08-09 16:47:21 +0200

    Added requirements.txt file for documentation generation

--- build_dist.bat
+++ build_dist.bat
@@ -1,6 +1,4 @@
 del MANIFEST
 python setup.py build sdist
-python setup.py --no-user-cfg build -c msvc bdist_wininst --sse2
 python setup.py --no-user-cfg build -c msvc bdist_wheel --sse2
-python setup.py --no-user-cfg build -c msvc bdist_wininst --plat-name=win32 --sse2
 python setup.py --no-user-cfg build -c msvc bdist_wheel --plat-name=win32 --sse2
