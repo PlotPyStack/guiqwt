652e1ddf6c86fc85e38b4a7d05dee5939393e2b6
Pierre Raybaut <pierre.raybaut@gmail.com>
2022-11-18 15:37:39 +0100

    build_doc.bat: fixed 7zip path

--- build_doc.bat
+++ build_doc.bat
@@ -2,13 +2,13 @@ sphinx-build -b htmlhelp doc doctmp
 "C:\Program Files\HTML Help Workshop\hhc.exe" doctmp\guiqwt.hhp
 "C:\Program Files (x86)\HTML Help Workshop\hhc.exe" doctmp\guiqwt.hhp
 copy doctmp\guiqwt.chm .
-7z a guiqwt.chm.zip guiqwt.chm
+"C:\Program Files\7-Zip\7z.exe" a guiqwt.chm.zip guiqwt.chm
 del doctmp\guiqwt.chm
 del doc.zip
 del doctmp\guiqwt.hh*
 sphinx-build -b html doc doctmp
 cd doctmp
-7z a -r ..\doc.zip *.*
+"C:\Program Files\7-Zip\7z.exe" a -r ..\doc.zip *.*
 cd ..
 rmdir /S /Q doctmp
 del guiqwt.chm.zip
